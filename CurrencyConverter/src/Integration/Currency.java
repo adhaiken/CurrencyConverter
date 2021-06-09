f4e3746008b6b20134536b42815ccf58f816589d
adhaiken <84459408+adhaiken@users.noreply.github.com>
2021-06-08 23:05:47 -0400

    Add files via upload

---
+++ CurrencyConverter/src/Integration/Currency.java
@@ -0,0 +1,13 @@
+package CurrencyConverter;
+
+public class Currency {
+	public String favCurrency;
+	  
+	public void setFavCurrency(String favCurrency) { // header and parameter
+	        this.favCurrency = favCurrency;
+	    }
+	    
+	public String getFavCurrency() {
+	        return this.favCurrency;
+	    }
+}    
